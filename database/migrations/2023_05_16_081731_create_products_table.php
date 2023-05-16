2083c09b670990071691bc27ee2c9499129fa6ec
OcidDev <ocid.asmanagement@gmail.com>
2023-05-16 16:52:07 +0700

    make model & migration category, rak, supplier, product

---
+++ database/migrations/2023_05_16_081731_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('categories_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('suppliers_id')->references('id')->on('suppliers')->onDelete('cascade');
+            $table->foreign('raks')->references('id')->on('raks')->onDelete('cascade');
+            $table->string('product_code');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('price')->unsigned();
+            $table->integer('modal')->unsigned();
+            $table->integer('stock')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
