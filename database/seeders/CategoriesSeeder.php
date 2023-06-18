99116d3f99f42efb73b4010a6e5bbec56845d1b9
OcidDev <ocid.asmanagement@gmail.com>
2023-06-18 22:40:52 +0700

    seeder image category

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use GuzzleHttp\Client;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CategoriesSeeder extends Seeder
@@ -15,59 +17,75 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        $categories = [
+
+        $data = [
             [
                 'name' => 'Makanan Ringan',
                 'description' => 'Kategori untuk makanan ringan',
-                'image' => 'makanan_ringan.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Minuman Botol',
                 'description' => 'Kategori untuk minuman botol',
-                'image' => 'minuman_botol.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Produk Kesehatan dan Kebersihan',
                 'description' => 'Kategori untuk produk kesehatan dan kebersihan',
-                'image' => 'kesehatan_kebersihan.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Bumbu Dapur',
                 'description' => 'Kategori untuk bumbu dapur',
-                'image' => 'bumbu_dapur.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Snack dan Cemilan',
                 'description' => 'Kategori untuk snack dan cemilan',
-                'image' => 'snack_cemilan.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Produk Susu dan Olahan Susu',
                 'description' => 'Kategori untuk produk susu dan olahan susu',
-                'image' => 'susu_olahan.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Minuman Sachet',
                 'description' => 'Kategori untuk minuman sachet',
-                'image' => 'minuman_sachet.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Produk Pembersih Rumah Tangga',
                 'description' => 'Kategori untuk produk pembersih rumah tangga',
-                'image' => 'pembersih_rumah_tangga.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Kecap, Saus, dan Bumbu Masak',
                 'description' => 'Kategori untuk kecap, saus, dan bumbu masak',
-                'image' => 'kecap_saus_bumbu_masak.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
             [
                 'name' => 'Produk Perawatan Pribadi',
                 'description' => 'Kategori untuk produk perawatan pribadi',
-                'image' => 'perawatan_pribadi.jpg',
+                'image' => 'https://img2.pngdownload.id/20180206/qoe/kisspng-popcorn-food-icon-yellow-popcorn-5a7a7951e4a064.1551013515179758899365.jpg',
             ],
         ];
+        $client = new Client();
+        foreach ($data as $item) {
+            $response = $client->request('GET', $item['image']);
+            $extension = pathinfo($item['image'], PATHINFO_EXTENSION);
+            $imageName = uniqid() . '.' . $extension;
+            Storage::put('public/assets/image/' . $imageName, $response->getBody());
+            $item['image'] = $imageName;
 
-        DB::table('categories')->insert($categories);
+            DB::table('categories')->insert(
+                [
+                    'name' => $item['name'],
+                    'description' => $item['description'],
+                    'image' => 'assets/image/'.$item['image'],
+                ]
+            );
+        }
+
     }
 }
